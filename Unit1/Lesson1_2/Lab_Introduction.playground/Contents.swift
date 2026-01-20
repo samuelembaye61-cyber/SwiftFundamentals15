e1415ff128d72b7b351aa52a27155dfc7498f407
Samuel Embaye <samuelembaye61@gmail.com>
2026-01-20 11:23:51 -0500

    update

---
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -0,0 +1,26 @@
+/*: 
+ ## Exercise: Use Playgrounds
+ 
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+ 
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+print("one love one heart bob marley.")
+
+
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+
+print("one love one heart ")
+print("let get together feel alright ")
+print("Hear the children crying (one love)")
+print("Hear the children crying (one heart)")
+//:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
