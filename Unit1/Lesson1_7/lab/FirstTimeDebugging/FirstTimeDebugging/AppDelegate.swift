f797b38cd7b2d60a4cc13c8d7017b80512cf591c
Samuel Embaye <samuelembaye61@gmail.com>
2026-01-28 10:59:40 -0500

    unit1/lesson1-7/

--- Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/AppDelegate.swift
+++ Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/AppDelegate.swift
@@ -15,9 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        var testArray: [String] = ["Uh oh..."]
+        var testArray: [String] = ["Uh oh...", "I'm fine!", "Just a test!"]
         testArray.removeFirst()
         testArray.removeFirst()
+        
         return true
     }
 
