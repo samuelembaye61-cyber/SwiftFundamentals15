de25716b592160bdb36892b80e455757be6b834a
Samuel Embaye <samuelembaye61@gmail.com>
2026-02-15 00:38:40 -0500

    unit2/lesson2-3/

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -8,10 +8,37 @@
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
 var steps = 0
+func incrementSteps()
+{
+    steps += 1
+    print(steps)
+}
 
 //:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
 let goal = 10000 
-
+func progressUpdate()
+{
+    ifsteps<goal/10
+    {
+        print("you're off to a good start")
+    }
+    else if steps<goal/2
+    {
+        print("you're almost halfway there")
+    }
+    else if steps<goal*9/10
+    {
+        print("you're over halfway there")
+    }
+    else if steps<goal
+    {
+        print("you're almost there")
+    }
+    else
+    {
+        print("you beat your goal")
+    }
+}
 
 /*:
 [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/6. App Exercise - Separating Functions.xcplaygroundpage/Contents.swift
@@ -7,10 +7,26 @@
 
  As an example, write a function that only does a portion of what your previous `pacing` function did. This function will be called `calculatePace`. It should take three `Double` arguments called `currentDistance`, `totalDistance`, and `currentTime`, and should return a `Double` that will represent the time at which the user will finish the run based on the user's current distance and time. call the function and print the return value.
  */
-
+func calculatePace(currentDistance:Double, totalDistance:Double, currentTime:Double) -> Double
+{
+    return currentDistance/totalDistance * currentTime
+}
+print(calculatePace(currentDistance: 3.0, totalDistance: 5.0, currentTime: 30.0))
 
 //:  Now write a function called `pacing` that takes four `Double` arguments called `currentDistance`, `totalDistance`, `currentTime`, and `goalTime`. The function should also return a `String`, which will be the message to show the user. The function should call `calculatePace`, passing in the appropriate values, and capture the return value. The function should then compare the returned value to `goalTime` and if the user is on pace return "Keep it up!", and return "You've got to push it just a bit harder!" otherwise. Call the function and print the return value.
-
+func pacing(currentDistance:Double, totalDistance:Double, currentTime:Double, goalTime:Double) -> String
+{
+    let pace = calculatePace(currentDistance: currentDistance, totalDistance: totalDistance, currentTime: currentTime)
+    if pace>goalTime
+    {
+        return "Keep it up!"
+    }
+    else
+    {
+        return "You've got to push it just a bit harder!"
+    }
+}
+print(pacing(currentDistance: 3.0, totalDistance: 5.0, currentTime: 30.0, goalTime: 25.0))
 
 /*:
  _Copyright © 2023 Apple Inc._
