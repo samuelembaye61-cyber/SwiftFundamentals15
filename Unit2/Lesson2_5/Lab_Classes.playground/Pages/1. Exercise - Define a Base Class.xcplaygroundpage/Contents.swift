e0bd043479d89693883b5b78051b7e17052eab74
Samuel Embaye <samuelembaye61@gmail.com>
2026-02-20 11:07:01 -0500

    x

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
@@ -5,16 +5,41 @@
 
  Create a `Spaceship` class with three variable properties: `name`, `health`, and `position`. The default value of `name` should be an empty string and `health` should be 0. `position` will be represented by an `Int` where negative numbers place the ship further to the left and positive numbers place the ship further to the right. The default value of `position` should be 0.
  */
- 
+class Spaceship{
+    var count: Int=0
+    var name: String = ""
+    var health:Int = 0
+    
+    func moveLeft(){
+        count-=1
+    }
+    func moveRight(){
+        count+=1
+    }
+    func wasHit(){
+        health-=1
+    }
+}
+    //:  Create a `let` constant called `falcon` and assign it to an instance of `Spaceship`. After initialization, set `name` to "Falcon."
+    let falcon = Spaceship()
+    falcon.name = "Falcon"
+    
+    //:  Go back and add a method called `moveLeft()` to the definition of `Spaceship`. This method should adjust the position of the spaceship to the left by one. Add a similar method called `moveRight()` that moves the spaceship to the right. Once these methods exist, use them to move `falcon` to the left twice and to the right once. Print the new position of `falcon` after each change in position.
+falcon.moveLeft()
+print(falcon.count)
+falcon.moveLeft()
+print(falcon.count)
+falcon.moveRight()
+print(falcon.count)
+    
+    //:  The last thing `Spaceship` needs for this example is a method to handle what happens if the ship gets hit. Go back and add a method `wasHit()` to `Spaceship` that will decrement the ship's health by 5, then if `health` is less than or equal to 0 will print "Sorry, your ship was hit one too many times. Do you want to play again?" Once this method exists, call it on `falcon` and print out the value of `health`.
+    if falcon.health >= 0{
+    print("Sorry, your ship was hit one too many times. Do you want to play again?")
+}
+else{ print("print heath") }
 
-//:  Create a `let` constant called `falcon` and assign it to an instance of `Spaceship`. After initialization, set `name` to "Falcon."
- 
-
-//:  Go back and add a method called `moveLeft()` to the definition of `Spaceship`. This method should adjust the position of the spaceship to the left by one. Add a similar method called `moveRight()` that moves the spaceship to the right. Once these methods exist, use them to move `falcon` to the left twice and to the right once. Print the new position of `falcon` after each change in position.
- 
-
-//:  The last thing `Spaceship` needs for this example is a method to handle what happens if the ship gets hit. Go back and add a method `wasHit()` to `Spaceship` that will decrement the ship's health by 5, then if `health` is less than or equal to 0 will print "Sorry, your ship was hit one too many times. Do you want to play again?" Once this method exists, call it on `falcon` and print out the value of `health`.
-
+falcon.wasHit()
+print(falcon.health)
 
 /*:
 page 1 of 4  |  [Next: Exercise - Create a Subclass](@next)
