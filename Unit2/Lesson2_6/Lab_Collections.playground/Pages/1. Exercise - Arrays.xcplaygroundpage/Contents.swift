5eec8f1bae83bfa6d5df94b2c9a54ad6511b1d5c
Samuel Embaye <samuelembaye61@gmail.com>
2026-02-22 17:18:22 -0500

    unit2/LEsson2_6/collection

--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -1,24 +1,24 @@
-/*:
-## Exercise - Arrays
- 
- Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
- */
 
 
 //:  Your friend Jasmine is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
-
+var registrationList: [String] = []
+registrationList.append("Jasmine")
+print(registrationList)
 
 //:  Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
-
-
+registrationList += ["Sam", "Isla", "Ava", "Mia"]
+print(registrationList)
 //:  Use the `insert(_:at:)` method to add `Trey` into the array as the second element. Print the contents of the collection.
-
-
+registrationList = Array(registrationList.prefix(2)) + ["Trey"] + Array(registrationList.suffix(from: 2))
+print(registrationList)
 //:  Somebody had a conflict and decided to transfer registration to someone else. Use array subscripting to change the sixth element to `Elton`. Print the contents of the collection.
-
-
+registrationList = Array(registrationList.prefix(5)) + ["Elton"] + Array(registrationList.suffix(from: 6))
+print(registrationList)
 //:  Call `removeLast()` on `registrationList`. If done correctly, this should remove `Elton` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
-
+registrationList.removeLast()
+let deletedItem = registrationList.removeLast()
+print(deletedItem)
+print(registrationList)
 
 /*:
 page 1 of 4  |  [Next: App Exercise - Activity Challenge](@next)
--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,22 +3,31 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-
-
+var monthDays: [String: Int] = ["january": 31, "february": 28, "march":31]
+print(monthDays)
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-
-
+monthDays["April"] = 30
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
+monthDays.updateValue(29, forKey: "february" )
 
+print(monthDays)
 
 //:  Use if-let syntax to retrieve the number of days under "January." If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
-
-
+let jandays = monthDays["january"]
+if((jandays) != nil) {
+print("January has \(jandays!) days")
+}
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
-
+var shapesArray: [String] = ["circle", "square", "triangle"]
+var colorsArray: [String] = ["red", "blue", "yellow"]
+var shapeColors: [String: [String]] = ["Shapes": shapesArray, "Colors": colorsArray]
+print(shapeColors)
 
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
-
+print(colorsArray.last!)
+if((shapeColors["Colors"]) != nil) {
+    shapeColors["Colors"]!.last!
+}
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
