9572f10cc9c2327b7ac02412833958d16201308c
Samuel Embaye <samuelembaye61@gmail.com>
2026-02-28 14:49:16 -0500

    unit2/Lesson2-9/cheesetutorial

---
+++ Unit2/Lesson2_9/cheeseGame/cheeseGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cheeseGame
+//
+//  Created by Samuel Embaye on 2/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit2/Lesson2_9/cheeseGame/cheeseGameTests/cheeseGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  cheeseGameTests.swift
+//  cheeseGameTests
+//
+//  Created by Samuel Embaye on 2/28/26.
+//
+
+import Testing
+@testable import cheeseGame
+
+struct cheeseGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
