1c2126e11ca5a9b1c8719c7801185fdf33fb4248
Samuel Embaye <samuelembaye61@gmail.com>
2026-03-01 17:47:11 -0500

    unit2lesson/2-11

---
+++ Unit2/Lesson2_11/calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Samuel Embaye on 3/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit2/Lesson2_11/calculator/calculatorTests/calculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorTests.swift
+//  calculatorTests
+//
+//  Created by Samuel Embaye on 3/1/26.
+//
+
+import Testing
+@testable import calculator
+
+struct calculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
